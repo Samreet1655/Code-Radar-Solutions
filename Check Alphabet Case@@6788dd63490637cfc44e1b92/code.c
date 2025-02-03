614f97b0b7af525772cb551e1c3f80a89d409eca
Samreet1655 <samreet1655.becse24@chitkara.edu.in>
2025-02-03 21:14:50 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main(){
-     char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" , str2[] = "abcdefghijklmnopqrstuvwxyz";
-     scanf("%d" , &a);
     char a;
+     char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" , str2[] = "abcdefghijklmnopqrstuvwxyz";
+     scanf("%c",a);
+  
     if (str1,a){
         printf("Uppercase");
     }
